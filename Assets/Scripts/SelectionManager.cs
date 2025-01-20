0c87ce317a056d13ceb82e2c8f6e05f9236d3301
shenburen <593446878@qq.com>
2025-01-20 16:03:40 +0800

    提交。

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -5,12 +5,31 @@ using UnityEngine.UI;
 
 public class SelectionManager : MonoBehaviour
 {
+    public static SelectionManager Instance
+    {
+        get; set;
+    }
+
+    public bool onTarget;
     public GameObject interaction_Info_UI;
     Text interaction_text;
 
     private void Start()
     {
+        onTarget = false;
         interaction_text = interaction_Info_UI.GetComponent<Text>();
+    }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
     }
 
     void Update()
@@ -21,18 +40,23 @@ public class SelectionManager : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
         {
             var selectionTransform = hit.transform;
-            if (selectionTransform.GetComponent<InteractableObject>())
+            InteractableObject interactable = selectionTransform.GetComponent<InteractableObject>();
+
+            if (interactable && interactable.playerInRange)
             {
-                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                onTarget = true;
+                interaction_text.text = interactable.GetItemName();
                 interaction_Info_UI.SetActive(true);
             }
             else
             {
+                onTarget = false;
                 interaction_Info_UI.SetActive(false);
             }
         }
         else
         {
+            onTarget = false;
             interaction_Info_UI.SetActive(false);
         }
     }
