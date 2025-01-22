f29ff5ca85f7cf6e6d258be3e6b35373b90de682
shenburen <593446878@qq.com>
2025-01-22 16:51:59 +0800

    提交。

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -11,6 +11,7 @@ public class SelectionManager : MonoBehaviour
     }
 
     public bool onTarget;
+    public GameObject selectedObject;
     public GameObject interaction_Info_UI;
     Text interaction_text;
 
@@ -45,6 +46,7 @@ public class SelectionManager : MonoBehaviour
             if (interactable && interactable.playerInRange)
             {
                 onTarget = true;
+                selectedObject = interactable.gameObject;
                 interaction_text.text = interactable.GetItemName();
                 interaction_Info_UI.SetActive(true);
             }
